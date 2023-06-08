5b79229c7979fe00eef81f9ace431152dd4ed47c
Marc Merzinger <marc@acloudjourney.io>
2023-06-08 11:57:30 +0200

    Extend demo app with exporter

--- tools/demo-app/webapp.py
+++ tools/demo-app/webapp.py
@@ -1,12 +1,16 @@
 import os
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='0.2.0')
 
 @app.route("/try-to-find-me/here-i-am")
 def hidden():
     return "<h1>You found the hidden endpoint :)</h1>"
 
 @app.route("/")
+@metrics.counter('http_requests_webroot', 'Invocations of webroot')
 def webroot():
     return "<h1>Hello from the demo app!</h1>"
 
